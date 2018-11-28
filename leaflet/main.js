aac504de838bf243542e6fe73b64db81f1ee420a
Lukas Belaj <lukasbelaj@gmail.com>
2018-11-28 15:46:15 +0100

    Update main.js
    Custom mapbox layer with higlighted buildings added

--- leaflet/main.js
+++ leaflet/main.js
@@ -55,7 +55,7 @@ function init(){
 }
 
 var mymap = L.map('mapid').setView([48.1406311, 17.1121223], 15);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/luigi32/cjp19pvu20zl22sp62522my5/tiles/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
